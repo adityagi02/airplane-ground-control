b99e220f740dd27ef177239ac1e9a36e24664a49
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2023-03-28 22:35:43 +0530

    Update DatePlannerApp.swift

--- DatePlannerApp.swift
+++ DatePlannerApp.swift
@@ -1,5 +1,5 @@
 //
-//  DatePlannerApp.swift
+//  DatePlanner.swift
 //  airplane ground control
 //
 //  Created by Aditya Tyagi  on 26/03/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct DatePlannerApp: View {
+struct DatePlanner: View {
     @StateObject private var eventData = EventData()
     
     var body: some View {
